b31a16028c2eb6460bb9707bfcbd14f589e99530
Sesóstris Vieira <sesostrisvieira@gmail.com>
2026-03-28 12:02:26 -0300

    Coloca no settings do tema algumas parametrizações para o frontpage

--- layout/frontpage_ilb.php
+++ layout/frontpage_ilb.php
@@ -31,8 +31,88 @@ $bodyattributes = $OUTPUT->body_attributes([]);
 
 $habilitar_destaque = $this->page->theme->settings->habilitar_destaque;
 $curso_destaque = $this->page->theme->settings->curso_destaque;
+$url_destaque = $this->page->theme->settings->url_destaque;
 $imagem_destaque = $this->page->theme->settings->imagem_destaque;
+$use_course_image = $this->page->theme->settings->use_course_image;
+$linha_marquet = format_text($this->page->theme->settings->linha_marquet, FORMAT_HTML);
+$video_institucional = $this->page->theme->settings->video_institucional;
+$signup = $this->page->theme->settings->signup;
+$forgotpwd = $this->page->theme->settings->forgotpwd;
+$course = get_course($curso_destaque);
 
+if (!$video_institucional) {
+	$video_institucional = "https://www.youtube.com/embed/mvqFvkBF0PE";
+}
+
+if (!$signup) {
+	$signup = "login/signup.php";
+}
+if (!$forgotpwd) {
+	$forgotpwd = "login/forgot_password.php";
+}
+if ($imagem_destaque) {
+	$imagem_destaque = $this->page->theme->setting_file_url('imagem_destaque', 'imagem_destaque');
+}
+
+if ($course) {
+	$courseurl = (new moodle_url('/course/view.php', ['id' => $course->id]))->out(false);
+} else {
+	$courseurl = null;
+}
+
+if (!$url_destaque) {
+	$url_destaque = $courseurl;
+}
+
+if ($use_course_image == 1 and $curso_destaque) {
+	if ($imagem_curso = \core_course\external\course_summary_exporter::get_course_image($course)) {
+		$imagem_destaque = $imagem_curso;
+	}
+}
+
+switch ($this->page->theme->settings->posicao_marquet) {
+	case 1: # Mostrar na parte superior
+		$marquet_superior = true;
+		$marquet_inferior = false;
+		break;
+	case 2: # mostrar na parte inferior
+		$marquet_superior = false;
+		$marquet_inferior = true;
+		break;
+	case 0: # Não mostrar em nenhum lugar
+	default:
+		$marquet_superior = false;
+		$marquet_inferior = false;
+		break;
+};
+
+$templatecontext = [
+	'sitename' => format_string($SITE->shortname, true, ['context' => context_course::instance(SITEID), "escape" => false]),
+	'output' => $OUTPUT,
+	'projetos_especiais' => $OUTPUT->image_url('projetos_especiais', 'theme'),
+	'conheca-senado' => $OUTPUT->image_url('conheca-senado', 'theme'),
+	'Cursos-on-line-sem-tutor' => $OUTPUT->image_url('Cursos-on-line-sem-tutor', 'theme'),
+	'Cursos-on-line' => $OUTPUT->image_url('Cursos-on-line', 'theme'),
+	'formacao_interna' => $OUTPUT->image_url('formacao_interna', 'theme'),
+	'oficinas-interlegis' => $OUTPUT->image_url('oficinas-interlegis', 'theme'),
+	'pos-graduacao' => $OUTPUT->image_url('pos-graduacao', 'theme'),
+	'video-aula' => $OUTPUT->image_url('video-aula', 'theme'),
+	'icon_ContatoEmail-azul' => $OUTPUT->image_url('icon_ContatoEmail-azul', 'theme'),
+	'icon_ContatoFone-azul' => $OUTPUT->image_url('icon_ContatoFone-azul', 'theme'),
+	'fundo-c' => $OUTPUT->image_url('fundo-c', 'theme'),
+	'logo_saberes_xl' => $OUTPUT->image_url('logo_saberes_xl', 'theme'),
+	'bodyattributes' => $bodyattributes,
+	'moodle_url' => $CFG->wwwroot,
+	'habilitar_destaque' => $habilitar_destaque,
+	'imagem_destaque' => $imagem_destaque,
+	'url_destaque' => $url_destaque,
+	'video_institucional' => $video_institucional,
+	'marquet_superior' => $marquet_superior,
+	'marquet_inferior' => $marquet_inferior,
+	'linha_marquet' => $linha_marquet,
+	'signup' => $signup,
+	'forgot-password' => $forgotpwd
+];
 
 if (isloggedin()) {
 	user_preference_allow_ajax_update('drawer-open-nav', PARAM_ALPHA);
@@ -54,24 +134,8 @@ if (isloggedin()) {
   	$user_profile_url=$CFG->wwwroot . "/user/profile.php?id=" . $USER->id . "&course=1";
 
   	$templatecontext = [
-	    'sitename' => format_string($SITE->shortname, true, ['context' => context_course::instance(SITEID), "escape" => false]),
-	    'output' => $OUTPUT,
-	    'projetos_especiais' => $OUTPUT->image_url('projetos_especiais', 'theme'),
-	    'conheca-senado' => $OUTPUT->image_url('conheca-senado', 'theme'),
-	    'Cursos-on-line-sem-tutor' => $OUTPUT->image_url('Cursos-on-line-sem-tutor', 'theme'),
-	    'Cursos-on-line' => $OUTPUT->image_url('Cursos-on-line', 'theme'),
-	    'formacao_interna' => $OUTPUT->image_url('formacao_interna', 'theme'),
-	    'oficinas-interlegis' => $OUTPUT->image_url('oficinas-interlegis', 'theme'),
-	    'pos-graduacao' => $OUTPUT->image_url('pos-graduacao', 'theme'),
-	    'video-aula' => $OUTPUT->image_url('video-aula', 'theme'),
-	    'icon_ContatoEmail-azul' => $OUTPUT->image_url('icon_ContatoEmail-azul', 'theme'),
-	    'icon_ContatoFone-azul' => $OUTPUT->image_url('icon_ContatoFone-azul', 'theme'),
-	    'fundo-c' => $OUTPUT->image_url('fundo-c', 'theme'),
-	    'fundo-c' => $OUTPUT->image_url('fundo-c', 'theme'),
-	    'logo_saberes_xl' => $OUTPUT->image_url('logo_saberes_xl', 'theme'),
 	   	'sidepreblocks' => $blockshtml,
  		'hasblocks' => $hasblocks,
-	    'bodyattributes' => $bodyattributes,
 	    'CursosOnlineSemTutoria' => $OUTPUT->image_url('Cursos-on-line-sem-tutor', 'theme'),
 	  	'navdraweropen' => $navdraweropen,
 	  	'regionmainsettingsmenu' => $regionmainsettingsmenu,
@@ -81,40 +145,13 @@ if (isloggedin()) {
 		'lastname' => $USER->lastname,
 		'sessKey' => $USER->sesskey,
 		'loginChangeNotification' => false,
-		'moodle_url' => $CFG->wwwroot,
 		'userpictureurl' => $user_picture_url,
 		'userprofileurl' => $user_profile_url,
-		'habilitar_destaque' => $habilitar_destaque,
-		'imagem_destaque' => $imagem_destaque,
-		'curso_destaque' => $curso_destaque
-	];
-
-
+	] + $templatecontext;
 } else {
-	
 	$templatecontext = [
-    	'sitename' => format_string($SITE->shortname, true, ['context' => context_course::instance(SITEID), "escape" => false]),
-    	'projetos_especiais' => $OUTPUT->image_url('projetos_especiais', 'theme'),
-	    'conheca-senado' => $OUTPUT->image_url('conheca-senado', 'theme'),
-	    'Cursos-on-line-sem-tutor' => $OUTPUT->image_url('Cursos-on-line-sem-tutor', 'theme'),
-	    'Cursos-on-line' => $OUTPUT->image_url('Cursos-on-line', 'theme'),
-	    'formacao_interna' => $OUTPUT->image_url('formacao_interna', 'theme'),
-	    'oficinas-interlegis' => $OUTPUT->image_url('oficinas-interlegis', 'theme'),
-	    'pos-graduacao' => $OUTPUT->image_url('pos-graduacao', 'theme'),
-	    'video-aula' => $OUTPUT->image_url('video-aula', 'theme'),
-	    'fundo-c' => $OUTPUT->image_url('fundo-c', 'theme'),
-	    'icon_ContatoEmail-azul' => $OUTPUT->image_url('icon_ContatoEmail-azul', 'theme'),
-	    'icon_ContatoFone-azul' => $OUTPUT->image_url('icon_ContatoFone-azul', 'theme'),
-	    'logo_saberes_xl' => $OUTPUT->image_url('logo_saberes_xl', 'theme'),
-	    'output' => $OUTPUT,
-	    'bodyattributes' => $bodyattributes,
-	    'moodle_url' => $CFG->wwwroot,
-	    'logintoken' => s(\core\session\manager::get_login_token()),
-		'habilitar_destaque' => $habilitar_destaque,
-		'imagem_destaque' => $imagem_destaque,
-		'curso_destaque' => $curso_destaque
-	];
-	
+	    'logintoken' => s(\core\session\manager::get_login_token())
+	] + $templatecontext;
 }
 
 $templatecontext['flatnavigation'] = $PAGE->flatnav;
--- settings.php
+++ settings.php
@@ -23,22 +23,96 @@
 defined('MOODLE_INTERNAL') || die();
 
 if ($ADMIN->fulltree) {
-    
+    // Bloco Acesso
+    $settings->add(new admin_setting_heading('theme_ilb/heading_acesso', 'Bloco de acesso', 'Links do bloco de acesso'));
+    // Signup url
+    $setting = new admin_setting_configtext(
+        "theme_ilb/signup",
+        "URL para página de cadastro",
+        "Entre a URL para a página de cadastro de novos usuários",
+        "login/signup.php",
+        PARAM_TEXT,
+        100
+    );
+    $settings->add($setting);
+    // Forgot password url
+    $setting = new admin_setting_configtext(
+        "theme_ilb/forgotpwd",
+        "URL para página de recuperação de senha",
+        "Entre a URL para a página de recuperação de senha",
+        "login/forgot_password.php",
+        PARAM_TEXT,
+        100
+    );
+    $settings->add($setting);
+        
+    // Destaque
+    $settings->add(new admin_setting_heading('theme_ilb/heading_destaque', 'Destaque', ''));
     // Habilitar destaque
     $setting = new admin_setting_configcheckbox('theme_ilb/habilitar_destaque',
         'Habilitar destaque',
         'Indica se deve ser exibido destaque na página inicial', 0);
     $settings->add($setting);
-
-    // Imagem destaque
-    $setting = new admin_setting_configtext('theme_ilb/imagem_destaque',
-        'Imagem de destaque',
-        'Imagem a ser exibida como destaque', '', PARAM_TEXT, 50);
-    $settings->add($setting);
-
     // Curso de destaque
     $setting = new admin_setting_configtext('theme_ilb/curso_destaque',
-        'Curso de destaque',
-        'ID do curso acessado ao clicar na imagem de destaque', '', PARAM_TEXT, 4);
+    'Curso de destaque',
+    'ID do curso acessado ao clicar na imagem de destaque', '', PARAM_TEXT, 4);
+    $settings->add($setting);
+    // URL de destque
+    $setting = new admin_setting_configtext(
+        "theme_ilb/url_destaque",
+        "URL de destaque",
+        "Uma URL externa para ser acessada ao clicar na imagem de destaque ao invés do curso. Esta URL tem precedência sobre o campo Curso de destaque.",
+        "",
+        PARAM_TEXT,
+        100
+    );
+    $settings->add($setting);
+    // Imagem destaque
+    $setting = new admin_setting_configstoredfile('theme_ilb/imagem_destaque',
+        'Imagem de destaque',
+        'Imagem a ser exibida como destaque',
+        'imagem_destaque',
+        0,
+        ["accepted_types" => [".png", ".jpg"]]);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $settings->add($setting);
+    // Qual imagem usar
+    $setting = new admin_setting_configcheckbox(
+        "theme_ilb/use_course_image",
+        "Usar imagem do curso",
+        "Se marcado, a imagem de capa do curso será usada no destaque",
+        "0");
+    $settings->add($setting);
+    // Vídeo institucional
+    $setting = new admin_setting_configtext(
+        "theme_ilb/video_institucional",
+        "Vídeo institucional",
+        "Entre a URL de um vídeo institucional para ser mostrado na linha de destaque,",
+        "https://www.youtube.com/embed/mvqFvkBF0PE",
+        PARAM_TEXT,
+        100
+    );
+    $settings->add($setting);
+
+    // Linha de marqueting
+    $settings->add(new admin_setting_heading('theme_ilb/heading_marquet', 'Marqueting', ''));
+    // Posição da linha
+    $setting = new admin_setting_configselect(
+        "theme_ilb/posicao_marquet",
+        "Posição da linha de marqueting",
+        "Indique se a linha de marqueting deve aparecer, e se antes ou depois da caixa de destaques",
+        0,
+        [0 => "Não mostrar a linha de marqueting",
+         1 => "Acima da linha de destaque",
+         2 => "Abaixo da linha de destaque"]);
+    $settings->add($setting);
+    // linha de marqueting
+    $setting = new admin_setting_confightmleditor(
+        'theme_ilb/linha_marquet',
+        'Linha de marqueting',
+        'trecho de HTML que que será renderizado na página inicial logo antes ou após a linha de destaques',
+        '',
+        PARAM_RAW);
     $settings->add($setting);
 }
